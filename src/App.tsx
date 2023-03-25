2fa2463d34bac0f0e0104b6b6e375ba085d77932
Keenan Southall <keenan.southall@yahoo.com>
2023-03-25 13:28:26 -0600

    Install router and cyress

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Grant Guru</h1>
-    </div>
+    <main className="App">
+      <Route path='/' render={() => <h1>Grant Guru</h1>}/>
+    </main>
   );
 }
 
-export default App;
+export default App
