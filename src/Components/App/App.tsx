35fd3a82dc198caa3e209191437b63d772574cdd
Keenan Southall <keenan.southall@yahoo.com>
2023-03-26 13:53:38 -0600

    Create modular component file structure with skeleton components

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Counter } from '../../features/counter/Counter';
 import './App.css';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <main className="App">
-      <Route path='/' render={() => <h1>Grant Guru</h1>}/>
+      <Route path='/' render={() => <h1>Grant Guru</h1>} />
     </main>
   );
 }
