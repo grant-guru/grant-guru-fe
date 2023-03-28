e609cebb1c34ce267681bbdc88c33019e8059107
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-28 17:28:26 -0400

    Clean up comments

--- src/slices/savedSlice.ts
+++ src/slices/savedSlice.ts
@@ -1,6 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../app/store';
-// import { fetchCount } from './SavedAPI';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface SavedState {
     saved: Array<any>
@@ -10,12 +8,6 @@ export interface SavedState {
 const initialState: SavedState = {
     saved: [],
 };
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 
 export const savedSlice = createSlice({
   name: 'saved',
@@ -39,13 +31,5 @@ export const savedSlice = createSlice({
 });
 
 export const { setSaved, addSaved, deleteSaved } = savedSlice.actions;
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.Saved.value)`
-
-
-// export const selectCount = (state: RootState) => state.Saved.value;
-
 
 export default savedSlice.reducer;
--- src/slices/scholarshipsSlice.ts
+++ src/slices/scholarshipsSlice.ts
@@ -1,6 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../app/store';
-// import { fetchCount } from './SavedAPI';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface ScholarshipsState {
     saved: Array<any>
@@ -10,12 +8,6 @@ export interface ScholarshipsState {
 const initialState: ScholarshipsState = {
     saved: [],
 };
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 
 export const scholarshipsSlice = createSlice({
   name: 'scholarships',
@@ -31,13 +23,5 @@ export const scholarshipsSlice = createSlice({
 });
 
 export const { setScholarships } = scholarshipsSlice.actions;
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.Saved.value)`
-
-
-// export const selectCount = (state: RootState) => state.Saved.value;
-
 
 export default scholarshipsSlice.reducer;
