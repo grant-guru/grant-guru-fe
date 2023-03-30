934f6599971bd5ec97b85413fa4e6b3b409f77d5
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-30 12:48:40 -0400

    Add local state for the username input

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,38 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css';
 import { useHistory } from 'react-router-dom';
 import { useAppDispatch } from "../../app/hooks";
 import { setUser } from "../../slices/userSlice";
 
-
-
 const Login = () => {
     const history = useHistory()
     const dispatch = useAppDispatch()
+
+    const [username, setUsername] = useState('')
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
         console.log('clicked')
+
         fetch('https://college-fund-mock-data-api.herokuapp.com/user')
         .then(response => response.json())
         .then(data => {
             dispatch(setUser(data))
-            console.log("this is the data",data)
-            console.log("")
+            console.log("this is the fetch data",data)
+            console.log("this is the username",username)
             history.push('/form')
         })
         .catch(error => console.log(error))
     }
-    
+    const handleInputChange = (e: any) => {
+        setUsername(e.target.value)
+    }
+
     return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
+        <div className="login-form-container">
+            <h1>Login</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input 
+                    type="text" 
+                    name="username" 
+                    id="username" 
+                    value={username}
+                    onChange={handleInputChange}
+                />
+                <br />
                 <button type="submit">Submit</button>
             </form >
-        </>
+        </div>
     )
 }
 
