8eddfabb6491850f5def9950593aa5b18c90c567
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-29 20:54:21 -0400

    Add the dispatch for setUser

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import './Login.css';
 import { useHistory } from 'react-router-dom';
+import { useAppDispatch } from "../../app/hooks";
+import { setUser } from "../../slices/userSlice";
 
 
 
 const Login = () => {
     const history = useHistory()
+    const dispatch = useAppDispatch()
     const handleSubmit = (e: any) => {
         e.preventDefault();
         console.log('clicked')
         fetch('https://college-fund-mock-data-api.herokuapp.com/user')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            dispatch(setUser(data))
+            console.log("this is the data",data)
+            console.log("")
             history.push('/form')
         })
         .catch(error => console.log(error))
