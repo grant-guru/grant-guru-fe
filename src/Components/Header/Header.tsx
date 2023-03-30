bafd7626fe05689147401d8d497c55ca936e777f
Jocelle Bautista <jcllebautista@gmail.com>
2023-03-29 22:27:26 -0400

    Route navigations

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import './Header.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return(
-        <>
-            <Link to={'/saved'}>
-                <button>saved</button>
-            </Link >
+const Header: React.FC = () => {
+    return (
+        <div className='header'>
+            <h1>Welcome User!</h1>
+            <nav>
+                  <Link to='/form'>Form</Link>
+                  <Link to='/scholarships'>Saves</Link>
+                  <Link to='/saved'>Scholarships</Link>
+            </nav>
+         </div>
+    );
+};
 
-            
-        </>
-    )
-}
-
-export default Header
+export default Header;
