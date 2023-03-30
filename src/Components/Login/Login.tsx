9e8303ab0ea0da4568b33e3c3b0b013c969b5f4b
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-29 20:44:15 -0400

    Add correct the Routes and redirect w the fetch

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,22 +1,32 @@
-import React, { useEffect } from "react";
+import React from "react";
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
+
 
 const Login = () => {
-    // useEffect(() => {
-    //     fetch('https://college-fund-mock-data-api.herokuapp.com/user')})
+    const history = useHistory()
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        console.log('clicked')
+        fetch('https://college-fund-mock-data-api.herokuapp.com/user')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            history.push('/form')
+        })
+        .catch(error => console.log(error))
+    }
     
     return(
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username</label>
                 <input type="text" name="username" id="username" />
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" />
-                <Link to={'/form'}>
-                    <button type="submit">Login</button>
-                </Link>
-            </form>
+                <button type="submit">Submit</button>
+            </form >
         </>
     )
 }
