c23b89cae3006b774bd5e932efb76cf0df6416dc
Jocelle Bautista <jcllebautista@gmail.com>
2023-04-04 19:22:55 -0400

    Refactor

--- src/Components/ScholarshipDetail/ScholarshipDetail.tsx
+++ src/Components/ScholarshipDetail/ScholarshipDetail.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './ScholarshipDetail.css';
 import Header from "../Header/Header";
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
@@ -6,51 +6,50 @@ import { addSaved, deleteSaved } from "../../slices/savedSlice";
 import { apiCalls } from "../../apiCalls";
 
 interface DetailProps {
-    id: string,
+  id: string;
+//   image_url: string;
 }
 
-const ScholarshipDetail = (props: DetailProps) => {
+const ScholarshipDetail: React.FC<DetailProps> = (props) => {
 
-    const { filtered } = useAppSelector(state => state.scholarships)
-    
-    const selectedScholarship = filtered.find(scholarship => scholarship.id === props.id)
+  const { filtered } = useAppSelector(state => state.scholarships);
+  const selectedScholarship = filtered.find(scholarship => scholarship.id === props.id);
 
-    const {saved} = useAppSelector(state => state.saved)
-    const dispatch = useAppDispatch()
-    const [isSaved, setSaved ] = useState(saved.some(save => save.id === props.id))
+  const { saved } = useAppSelector(state => state.saved);
+  const dispatch = useAppDispatch();
+  const [isSaved, setSaved] = useState(saved.some(save => save.id === props.id));
 
-    const handleAdd = () => {
-        if(!isSaved) {
-            dispatch(addSaved(props))
-            setSaved(true)
-        } else {
-            dispatch(deleteSaved(props))
-            setSaved(false)
-        }
-        // apiCalls.addSavedScholarship(user.id, props.id)
-        //     .then(json => console.log(json))
-        //     .catch(err => console.log(err))
+  const handleAdd = () => {
+    if (!isSaved) {
+      dispatch(addSaved(props));
+      setSaved(true);
+    } else {
+      dispatch(deleteSaved(props));
+      setSaved(false);
     }
- 
-    return(
-        <>
-            <Header />
-            <div className='scholarshipDetail'>
-                <h1> {selectedScholarship?.attributes.title} </h1>
-                <body style={{
-                backgroundImage: `url(${selectedScholarship?.attributes.image_url})`,
-                }}>
-                    <div className="background-gradient">
-                    <h2> ${selectedScholarship?.attributes.amount} </h2>
-                    <p> {selectedScholarship?.attributes.description} </p>
-                    <h4> <label>Deadline for submission:</label><br/>{selectedScholarship?.attributes.deadline} </h4>
-                    <button className="scholarship-button" onClick={() => handleAdd()} style={{ backgroundColor: isSaved ? "red" : "green"}}>{isSaved ? "Remove from Saved" : "Save this Scholarship"}</button>
-                    </div>
-                </body>
-            </div>
-        </>
+    // apiCalls.addSavedScholarship(user.id, props.id)
+    //     .then(json => console.log(json))
+    //     .catch(err => console.log(err))
+  }
 
-    )
+  return (
+    <>
+      <Header />
+      <div className='scholarshipDetail'>
+        <h1> {selectedScholarship?.attributes.title} </h1>
+        <body style={{
+          backgroundImage: `url(${selectedScholarship?.attributes.image_url})`,
+        }}>
+          <div className="background-gradient">
+            <h2> ${selectedScholarship?.attributes.amount} </h2>
+            <p> {selectedScholarship?.attributes.description} </p>
+            <h4> <label>Deadline for submission:</label><br/>{selectedScholarship?.attributes.deadline} </h4>
+            <button className="scholarship-button" onClick={() => handleAdd()} style={{ backgroundColor: isSaved ? "red" : "green"}}>{isSaved ? "Remove from Saved" : "Save this Scholarship"}</button>
+          </div>
+        </body>
+      </div>
+    </>
+  );
 }
-    
-export default ScholarshipDetail
+
+export default ScholarshipDetail;
