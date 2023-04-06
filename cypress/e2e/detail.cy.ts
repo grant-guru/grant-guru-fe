de2adff4492bc03ca5d9637c782462a0713ba600
Jocelle Bautista <jcllebautista@gmail.com>
2023-04-05 23:46:13 -0400

    Refactor intercepts

--- cypress/e2e/detail.cy.ts
+++ cypress/e2e/detail.cy.ts
@@ -3,32 +3,34 @@
 describe("should navigate to scholarships page on form submit", () => {
   beforeEach(() => {
     cy.visit("https://grant-guru-fe.vercel.app/");
-    cy.intercept("GET", "https://college-fund-mock-data-api.herokuapp.com/user", {
+    cy.intercept("GET", "https://grant-guru-be.herokuapp.com/api/v1/users/1/", {
       fixture: "user.json",
     });
+    cy.intercept("GET", "https://grant-guru-be.herokuapp.com/api/v1/scholarships/5", { fixture: 'scholarships.json'});
+    cy.intercept("GET", "https://grant-guru-be.herokuapp.com/api/v1/users/1/favorites", { fixture: 'saved.json'});
+    cy.intercept("GET", "https://grant-guru-be.herokuapp.com/api/v1/scholarships", { fixture: 'scholarships.json'});
     cy.get(".user-card-container").first().click();
-
-    cy.intercept("GET", "https://college-fund-mock-data-api.herokuapp.com/scholarships", { fixture: 'scholarships.json'});
-    cy.intercept("GET", "https://college-fund-mock-data-api.herokuapp.com/favorites", { fixture: 'saved.json'});
     cy.get("button.form-submit").click();
+
     cy.location("pathname").should("eq", "/scholarships");
     cy.get('.Library > :nth-child(2)').click()
-    cy.location('pathname').should('eq', '/scholarship/5')
+    cy.location('pathname').should('eq', '/scholarship/2')
   });
 
   it("should display scholarship detail", () => {
-    cy.get('.scholarshipDetail').contains("Johnson-Baldwin Grant");
-    cy.get('h2').contains('$971');
+    cy.get('.scholarshipDetail').contains("RailsConf 23");
+    cy.get('h2').contains('$1234');
     cy.get('h4').contains('label', 'Deadline for submission:')
   })
 
-  // it("Should be able to save button and the button character changes", () => {
-  //   cy.get('.scholarship-button').contains("Remove from Saved").click()
-  //   cy.get('.scholarship-button').contains("Save this Scholarship").click()
-  // })
+  it("Should be able to save button and the button character changes", () => {
+    cy.get('.scholarship-button').contains("Save this Scholarship").click()
+    cy.get('.scholarship-button').contains("Remove from Saved").click()
+    cy.get('.scholarship-button').contains("Save this Scholarship").click()
+  })
 
-  // it("Should be able to add to saved array", () => {
-  //   cy.get('.saved').click()
-  //   cy.get('.Library > :nth-child(4)').should('be.visible')
-  //});
+  it("Should be able to add to saved array", () => {
+    cy.get('.saved').click()
+    cy.get('.Library > :nth-child(1)').should('be.visible')
+  });
 })
