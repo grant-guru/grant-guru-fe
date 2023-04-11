396a48cb96902d5c6066843e7f65794841201c5f
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-11 12:26:22 -0400

    Update the hosting url for the images

--- src/data/data.js
+++ src/data/data.js
@@ -57,7 +57,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Drew",
             "last_name": "Layton",
-            "image_url": "https://media.licdn.com/dms/image/D5635AQHEnwpJLXt26Q/profile-framedphoto-shrink_800_800/0/1678064386468?e=1680879600&v=beta&t=p8ZKUqplSskK5g-2phNbRo1UgXLdLMAZ0X_lmdowZfs"
+            "image_url": "https://drive.google.com/uc?id=190h7SwaDw8if6QBRE0VNSx4fmiF70-G4"
         }
     },
     {
@@ -66,7 +66,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Kaylah Rose",
             "last_name": "Mitchell",
-            "image_url": "https://media.licdn.com/dms/image/D5603AQF267qmv4H5LA/profile-displayphoto-shrink_800_800/0/1675731750982?e=1685577600&v=beta&t=j9AGMADYpyBgk6ez01xqivshVOADLvPjGJBs6woSLVQ"
+            "image_url": "https://drive.google.com/uc?id=1tgK-ZohMiYw9lhaseH-pliwnIrYkQuN7"
         }
     },
     {
@@ -75,7 +75,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Sergio",
             "last_name": "Azcona",
-            "image_url": "https://media.licdn.com/dms/image/C5603AQFg36SHA4Yaow/profile-displayphoto-shrink_800_800/0/1655168145023?e=1685577600&v=beta&t=7HIj5uv3UqIPvspsiIpPDIvJfr0DhNAzVezFZ6SBsTs"
+            "image_url": "https://drive.google.com/uc?id=1I5YU8mSs7LpMVIvjeWr0AvMKotW0loj6"
         }
     }, 
     {
@@ -84,7 +84,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Matisse",
             "last_name": "Mallette",
-            "image_url": "https://media.licdn.com/dms/image/C5603AQGZliXUratj0Q/profile-displayphoto-shrink_800_800/0/1663354238434?e=1685577600&v=beta&t=0Tbn6cOH3rp45302bhx2oanUXtrvC1XtDdvmEpScUpw"
+            "image_url": "https://drive.google.com/uc?id=1WMEYhLO0bRbTy3Bkeq-D3TWMpEXfHUid"
         }
     },
     {
@@ -93,7 +93,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Keenan",
             "last_name": "Southall",
-            "image_url": "https://media.licdn.com/dms/image/D4E03AQHgAoRoOzOChQ/profile-displayphoto-shrink_800_800/0/1665629022125?e=1685577600&v=beta&t=UmtbW5x5VcrvtDl45ngIvA0f1B9E_hRDH1JDZgHcj3A"
+            "image_url": "https://drive.google.com/uc?id=1B2sLstsGSHQwHHyLX_hEaO4cejANSSdB"
         }
     },
     {
@@ -102,7 +102,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Reid",
             "last_name": "Poole",
-            "image_url": "https://media.licdn.com/dms/image/D5635AQF3PxKiLVYhcw/profile-framedphoto-shrink_800_800/0/1679083658885?e=1680883200&v=beta&t=XpcpJSCPzhfxR-RLDg--6CCQHIv6u0BdV7ooCmOs5Jo"
+            "image_url": "https://drive.google.com/uc?id=1Hi8YMMi9MLC362dpdumQsIWoc5HjEViV"
         }
     },{
         "id": "7",
@@ -110,7 +110,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Jocelle",
             "last_name": "Bautista",
-            "image_url": "https://media.licdn.com/dms/image/D5635AQEIHVN77x1QAw/profile-framedphoto-shrink_800_800/0/1678550742745?e=1680879600&v=beta&t=ieS0A5xEOTfyWAToumQR3iE9CmfucWFDsx1S1xCqDdo"
+            "image_url": "https://drive.google.com/uc?id=15mPEEsyrdV3eoTpQFE6x19ewrZuj2BCD"
         }
     }, 
     {
@@ -119,7 +119,7 @@ export const usStates = [
         "attributes": {
             "first_name": "Adam",
             "last_name": "Hughes",
-            "image_url": "https://media.licdn.com/dms/image/C5603AQHvilQI3AUEMQ/profile-displayphoto-shrink_800_800/0/1609862815851?e=1685577600&v=beta&t=HMl5xDZTacnO-E1fQSgfzhgzCJKMBc3x-pj5LaSZA-M"
+            "image_url": "https://drive.google.com/uc?id=1N8tJYNRWlrlbd9LxBxt4d6HWSragDaUw"
         }
     }
     ]
